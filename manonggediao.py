60f2d8e943b4c8333a5d57c12fc226bea730303a
domon <domonji95@gmail.com>
2016-08-16 14:23:41 +0800

    first

--- manonggediao.py
+++ manonggediao.py
@@ -7,21 +7,21 @@ app = Flask(__name__)
 app.debug=True
 
 @app.route('/', methods=['GET'])
- def weixin_verify():
-     signature = request.args.get('signature')
-     timestamp = request.args.get('timestamp')
-     nonce = request.args.get('nonce')
-     echostr = request.args.get('echostr')
+def weixin_verify():
+    signature = request.args.get('signature')
+    timestamp = request.args.get('timestamp')
+    nonce = request.args.get('nonce')
+    echostr = request.args.get('echostr')
 
-     token = r'jixiaomeng' #和申请消息接口时的Token一致
-     tmplist = [token, timestamp, nonce]
-     tmplist.sort()
-     tmpstr = ''.join(tmplist)
-     hashstr = hashlib.sha1(tmpstr).hexdigest()
+    token = r'jixiaomeng' #和申请消息接口时的Token一致
+    tmplist = [token, timestamp, nonce]
+    tmplist.sort()
+    tmpstr = ''.join(tmplist)
+    hashstr = hashlib.sha1(tmpstr).hexdigest()
 
-     if hashstr == signature:
-         return make_response(echostr) #success
-     return 'access verification fail' #fail
+    if hashstr == signature:
+        return make_response(echostr) #success
+    return 'access verification fail' #fail
 
 if __name__=='__main__':
     app.run()
