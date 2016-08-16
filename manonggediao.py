993154af8d57de5717810f65b59c94b8b05cb2f0
domon <domonji95@gmail.com>
2016-08-16 14:50:47 +0800

    first

--- manonggediao.py
+++ manonggediao.py
@@ -1,29 +1,30 @@
 #coding:utf-8
-from flask import Flask, g, request,make_response
-import time
-import hashlib
-import xml.etree.ElementTree as ET
+from flask import Flask, request, make_response
+from hashlib import sha1
 
 app = Flask(__name__)
-app.debug=True
 
-@app.route('/', methods=['GET'])
-def weixin_verify():
-    signature = request.args.get('signature')
-    timestamp = request.args.get('timestamp')
-    nonce = request.args.get('nonce')
-    echostr = request.args.get('echostr')
-    token = 'jixiaomeng' #和申请消息接口时的Token一致
-    tmplist = [token, timestamp, nonce]
-    tmplist.sort()
-    tmpstr = ''.join([str(s) for s in tmplist])
-    hashstr = hashlib.sha1(tmpstr).hexdigest()
 
-    return str(echostr)
+@app.route('/')
+def index():
+    return "Hello,World!"
 
-    if hashstr == signature:
-        return make_response(echostr) #success
-    return 'access verification fail'#fail
 
-if __name__=='__main__':
+@app.route('/check',  methods=['GET', 'POST'])
+def check():
+    if request.method == 'GET':
+        token = r'jixiaomeng' # 这个根据自己的设置自行修改
+        signature = request.args.get('signature', '')
+        echostr = request.args.get('echostr', '')
+        timestamp = request.args.get('timestamp', '')
+        nonce = request.args.get('nonce', '')
+        tmp = [timestamp, nonce, token]
+        tmp.sort()
+        tmp = ''.join(tmp)
+        if signature == sha1(tmp).hexdigest():
+            return  make_response(echostr)
+        else:
+            return "Access denied."
+
+if __name__ == '__main__':
     app.run()
